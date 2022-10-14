639471d538000dc1107297d27c19ba47985d6b79
Helen <helenmperham@gmail.com>
2022-10-14 15:59:12 -0400

    commit tree ec88772256a61427b1787e32a9bf5fbf5980e40d

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    "hello".toUpperCase(); // 'HELLO!'
+    console.log('HELLO');
+}
+function logWhisper(string) {
+    "hello".toLowerCase(); // 'hello!'
+    console.log('hello');
+}
+function sayHiToHeadphonedRoommate(string){
+    if (string==="let's have dinner together");
+    return ("I can't hear you!");
+    {else if (string==="LET'S HAVE DINNER TOGETHER!");
+    return ("YES INDEED!")};
+    {else if (string==="Let's have dinner together!");
+    return ("I would love to!")};
+}
